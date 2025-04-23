a5a945733ce693cd6c0c9787e2fd225bcf0377d6
Lisxt <alice14072005@gmail.com>
2025-04-23 18:51:49 -0300

    Projeto Programação web

---
+++ js.js
@@ -0,0 +1,8 @@
+//EXEMPLOS DE ONDE UTILIZAR
+
+//Manipulação do DOM - Para mostrar e esconder telas (boas-vindas, quiz, resultado), capturar respostas dos botões, exibir o resultado final.
+//Funções (normais ou arrow) - organizar ações do código, como calcular pontuação, avançar perguntas, mostrar resultado etc.
+//Condicionais (if/else/switch) - decidir qual personagem ganhou, o que mostrar com base nas escolhas do usuário, ou ações baseadas em ID de perguntas.
+//Laços de repetição (for/while) - percorrer as perguntas e opções, somar pontuações, ou gerar elementos na interface dinamicamente.
+//Listas e objetos - guardar perguntas, personagens, respostas e pontuações.
+//Orientação a objetos (classes) - representar personagens, encapsular dados e comportamentos.
